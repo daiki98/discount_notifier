e50eeb1e41bc527c2950807fac6239f06e981361
daiki98 <zdymori@gmail.com>
2022-05-04 08:50:49 -0400

    deleted .history

--- .history/get_api_token_20220504072416.py
+++
@@ -1,4 +0,0 @@
-def get_api_token():
-    return recover_token('SL5aUquBVVDu6kGq3l3evkuSTSoHnQtj1rRybGhxg3n')
-
-def recover_token():
--- .history/get_api_token_20220504074823.py
+++
@@ -1,2 +0,0 @@
-def get_token():
-    return 'SL5aUquBVVDu6kGq3l3evkuSTSoHnQtj1rRybGhxg3n'
